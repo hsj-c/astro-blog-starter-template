9089f2eddd653ae6792070855638646c7622c2c8
taka <56256994+takanori-matsushita@users.noreply.github.com>
2026-06-09 01:00:37 +0900

    準備中

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "放課後等デイサービスLim";
+export const SITE_DESCRIPTION = "鹿児島市中山で運営を行っています。当施設では、子どもたちの可能性を広げるため、療育以外にも様々な活動を積極的に行っています。";
